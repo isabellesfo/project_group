f1243b757a42bcb639c5b4750f10ada2f38476b9
isabellesfo <s10257592@connect.np.edu.sg>
2023-08-03 23:02:05 +0800

    p and l edit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #imports the respective dictionaries into the main folder
 from overheads import overhead_data, find_highest_overhead
 from cash_on_hand import cash_data, calculate_cash_surplus_deficit
-from profit_loss import (
+from profit_and_loss import (
     profit_and_loss,
     calculate_profit_loss_scenario2,
     calculate_profit_loss_scenario3,
