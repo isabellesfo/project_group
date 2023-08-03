fade687d1494c1e86550d2fbcdef2001a3b96cde
isabellesfo <s10257592@connect.np.edu.sg>
2023-08-03 22:59:51 +0800

    heeee

--- overheads.py
+++ overheads.py
@@ -10,7 +10,7 @@ csv_folder = os.path.join(os.getcwd(), "csv_reports")
 os.chdir(csv_folder)
 
 # Now you can open and read the CSV file
-csv_file = "overheads.csv"
+csv_file = "Overheads.csv"
 
 with open(csv_file, mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
