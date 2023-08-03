c08dbc993e4d7887c90415ea2abe5beb2ed78911
isabellesfo <s10257592@connect.np.edu.sg>
2023-08-03 22:41:15 +0800

    main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello")
+# main.py
+from overheads import overhead_data, find_highest_overhead
+from cash_on_hand import cash_data, calculate_cash_surplus_deficit
+from profit_loss import (
+    profit_and_loss,
+    calculate_profit_loss_scenario2,
+    calculate_profit_loss_scenario3,
+    find_highest_net_profit_surplus,
+)
+
