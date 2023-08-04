b29ffe47857ef8353171bbf67af1679914b1f683
isabellesfo <s10257592@connect.np.edu.sg>
2023-08-05 00:48:19 +0800

    annotations

--- Cash_On_Hand.py
+++ Cash_On_Hand.py
@@ -17,29 +17,43 @@ csv_file= "Cash_On_Hand.csv"
 with open(csv_file, mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
 
-# This skips the header row as it contains column names
+# This skips the header row as it contains column names that are not needed. 
     next(reader)
 
+# This assigns an empty list to the variable cash_data. 
     cash_data = []
 
+# This is a loop that will execute the code for each row of data in reader. 
     for row in reader:
+
+# This appends the list for the code and creates a new list with a converted integer and float value
         cash_data.append([int(row[0]), float(row[1])])
 
-# Calculate Cash Surplus or Cash Deficit
+# This helps defines the function "calculate_cash_surplus_deficit" with the parameter "data".
 def calculate_cash_surplus_deficit(data):
+
+# This lines initialises the variable with the value "False", assuming there is no cash until proven later on in the code.
     has_cash_surplus = False
     has_cash_deficit = False
 
+# This is a loop that will execute the code for the data in the range. 
     for i in range(1, len(data)):
+
+# This line assigns the value of the second elemnt of the 'i' in the "data" list to the variable "current_cash".
         current_cash = data[i][1]
+        
+# This line assigns the value of the second elemnt of the 'i-i' in the "data" list to the variable "previous_cash".
         previous_cash = data[i - 1][1]
 
+# This line checks if the "current_cash" value is greater than the "previous_cash" and if it is "True", the code under this"if" function will run.
         if current_cash > previous_cash:
             has_cash_surplus = True
             print(f"[CASH SURPLUS] DAY: {data[i][0]}, AMOUNT: {current_cash - previous_cash}")
 
+# This line checks if the "current_cash" value is smaller than the "previous_cash" and if it is "True", the code under this "if" function will run.
         elif current_cash < previous_cash:
             has_cash_deficit = True
             print(f"[CASH DEFICIT] DAY: {data[i][0]}, AMOUNT: {previous_cash - current_cash}")
 
+# This line changes the directory from the operating system back to the original.
 os.chdir(original_working_dir)
