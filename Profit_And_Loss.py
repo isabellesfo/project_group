cdfe8931277e05be670895012a6e8472c42b8dfa
isabellesfo <s10257592@connect.np.edu.sg>
2023-08-06 23:38:39 +0800

    p and loss annotate

--- Profit_And_Loss.py
+++ Profit_And_Loss.py
@@ -70,54 +70,86 @@ def calculate_profit_loss_scenario1(data):
             if i > 0 and data[i][4] > data[i - 1][4]:
                 print("[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
 
-#
+# This line finds the highest net profit surplus from "data".
+# The max() function is used to find the maximum value based on the fifth element of the sublist in "data" and the amount is stored in the "highest_net_profit_surplus" variable.
         highest_net_profit_surplus = max(data, key=lambda x: x[4])[4]
         print(f"[HIGHEST NET PROFIT SURPLUS] DAY: {data[-1][0]}, AMOUNT: {highest_net_profit_surplus}")
 
-# Calculate Profit and Loss for Scenario 2 - Constant Profit Deficit Everyday
+# This line takes the "data" as input.
 def calculate_profit_loss_scenario2(data):
+
+# This function initialises a boolean variable "is_profit_deficit" and sets it to "True". This variable will be used to track if theres a deficit or not.
     is_profit_deficit = True
+
+# This line of code is a loop that iterates over the "data" using the indices and extracts the profit or loss amount for each day and stores it in the "profit" variable.
     for i in range(len(data)):
         profit = data[i][2]
+
+# This line of code checks if "profit" is greater than or equal to zero. 
+# If there is a positive profit on that day, it sets "is_profit_decifit" to "False"
         if profit >= 0:
             is_profit_deficit = False
             break
 
+# This line of code checks the value of "is_profit_deficit", if it is true, all the days in "data" would have a deficit.
     if is_profit_deficit:
         
+# This line of code executes only if "is_profit_deficit" is true.
+# This line calculates the deficit amount for each day.
         for i in range(len(data)):
             deficit = data[i][2] * -1
             print(f"DAY: {data[i][0]}, AMOUNT: {deficit}")
 
-# Calculate Profit and Loss for Scenario 3 - Fluctuation between Profit Surplus and Profit Deficit
+# This line takes the "data" as input.
 def calculate_profit_loss_scenario3(data):
+
+# This line of code initialises a boolean variable "has_profit_surplus" and sets it to "False". This function tracks if there is any day with a positive profit surplus.
     has_profit_surplus = False
+
+# This line of code initialises a boolean variable "has_profit_deficit" and sets it to "False". This function tracks if there is any day with a negative profit deficit.
     has_profit_deficit = False
 
     for i in range(len(data)):
         profit = data[i][2]
+
+# This line of code checks if "profit" is greater than zero, if it is, it set "has_profit_surplus" to "True".
         if profit > 0:
             has_profit_surplus = True
+
+# This line of code checks if "profit" is smaller than zero, if it is, it set "has_profit_deficit" to "True".
         elif profit < 0:
             has_profit_deficit = True
 
+# This code checks the values of "has_profit_surplus" and "has_profit_deficit". If both of them are True, it means there are days with both positive profit surplus and negative profit deficit.
     if has_profit_surplus and has_profit_deficit:
         
         for i in range(len(data)):
             profit = data[i][2]
+
+# This line of code checks if "profit" is greater than zero, if it is, it prints the statement "[PROFIT SURPLUS] DAY: {data[i][0]}, AMOUNT: USD {profit}".
             if profit > 0:
                 print(f"[PROFIT SURPLUS] DAY: {data[i][0]}, AMOUNT: USD {profit}")
+
+# This line of code checks if "profit" is smaller than zero, if it is, calculates the deficit amount by multiplying the profit/loss amount by -1 and prints the statement ""[PROFIT DEFICIT] DAY: {data[i][0]}, AMOUNT: USD {deficit}".
             elif profit < 0:
                 deficit = data[i][2] * -1
                 print(f"[PROFIT DEFICIT] DAY: {data[i][0]}, AMOUNT: USD {deficit}")
 
-# Find Highest Net Profit Surplus without using abs()
+# This line takes the "data" as input.
 def find_highest_net_profit_surplus(data):
+
+# This line of code initialises a variable "highest_net_profit_surplus" and sets it to the net profit of the first day in "data".
+# This value serves as the initial highest net profit surplus. 
     highest_net_profit_surplus = data[0][4]
     for i in range(1, len(data)):
+
+# This line of code extracts the net profit for the current and stores it in the variable "net_profit".
         net_profit = data[i][4]
+
+# This line of code checks if the "net_profit" for the current day is greater than the "highest_net_profit_surplus" and updates the "highest_net_profit_surplus" variable with a new value.
         if net_profit > highest_net_profit_surplus:
             highest_net_profit_surplus = net_profit
     return highest_net_profit_surplus
 
+# This line changes the directory from the operating system back to the original.
 os.chdir(original_working_dir)
