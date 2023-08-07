e7a9809d92f5756385a1b64d6601621ad0116a2b
isabellesfo <s10257592@connect.np.edu.sg>
2023-08-07 19:18:12 +0800

    coh

--- Cash_On_Hand.py
+++ Cash_On_Hand.py
@@ -3,15 +3,14 @@ import os
 from pathlib import Path
 import csv
 
-# This stores the current working directory in the variable.
 original_working_dir= os.getcwd()
 
-# This sets the current working directory to the folder containing the CSV file.
-csv_folder= os.path.join(os.getcwd(), "csv_reports")
+# This lines sets the current working directory to the folder containing the CSV file. 
+csv_folder = os.path.join(os.getcwd(), "csv_reports")
 os.chdir(csv_folder)
 
-# The variable that stores the name of the csv file, "Cash_On_Hand.csv" that the code will read.
-csv_file= "Cash_On_Hand.csv"
+# This line opens and reads the CSV file.
+csv_file = "Cash_On_Hand.csv"
 
 # This opens and reads the csv file.
 with open(csv_file, mode="r", encoding="UTF-8", newline="") as file:
@@ -20,7 +19,8 @@ with open(csv_file, mode="r", encoding="UTF-8", newline="") as file:
 # This skips the header row as it contains column names that are not needed. 
     next(reader)
 
-# This assigns an empty list to the variable cash_data. 
+# This assigns an empty list to the variable overhead_data. 
+
     cash_data = []
 
 # This is a loop that will execute the code for each row of data in reader. 
@@ -29,31 +29,38 @@ with open(csv_file, mode="r", encoding="UTF-8", newline="") as file:
 # This appends the list for the code and creates a new list with a converted integer and float value
         cash_data.append([int(row[0]), float(row[1])])
 
-# This helps defines the function "calculate_cash_surplus_deficit" with the parameter "data".
+# Calculate Cash Surplus or Cash Deficit and Find Largest Increment or Deficit
+# Calculate Cash Surplus or Cash Deficit and Find Largest Increment or Deficit
 def calculate_cash_surplus_deficit(data):
+    largest_increment = 0
+    largest_increment_day = None
+    largest_deficit = 0
+    largest_deficit_day = None
 
-# This lines initialises the variable with the value "False", assuming there is no cash until proven later on in the code.
-    has_cash_surplus = False
-    has_cash_deficit = False
-
-# This is a loop that will execute the code for the data in the range. 
     for i in range(1, len(data)):
-
-# This line assigns the value of the second elemnt of the 'i' in the "data" list to the variable "current_cash".
         current_cash = data[i][1]
-        
-# This line assigns the value of the second elemnt of the 'i-i' in the "data" list to the variable "previous_cash".
         previous_cash = data[i - 1][1]
+        increment = current_cash - previous_cash
 
-# This line checks if the "current_cash" value is greater than the "previous_cash" and if it is "True", the code under this"if" function will run.
+        if increment > largest_increment:
+            largest_increment = increment
+            largest_increment_day = data[i][0]
+
+        if increment < largest_deficit:
+            largest_deficit = increment
+            largest_deficit_day = data[i][0]
+
         if current_cash > previous_cash:
-            has_cash_surplus = True
-            print(f"[CASH SURPLUS] DAY: {data[i][0]}, AMOUNT: {current_cash - previous_cash}")
-
-# This line checks if the "current_cash" value is smaller than the "previous_cash" and if it is "True", the code under this "if" function will run.
+            print(f"[CASH SURPLUS] DAY: {data[i][0]}, AMOUNT: USD{increment}")
         elif current_cash < previous_cash:
-            has_cash_deficit = True
-            print(f"[CASH DEFICIT] DAY: {data[i][0]}, AMOUNT: {previous_cash - current_cash}")
+            print(f"[CASH DEFICIT] DAY: {data[i][0]}, AMOUNT: USD{-increment}")
+
+    if largest_increment_day is not None:
+        print(f"[LARGEST INCREMENT] DAY: {largest_increment_day}, AMOUNT: USD{largest_increment}")
+
+    if largest_deficit_day is not None:
+        print(f"[LARGEST DEFICIT] DAY: {largest_deficit_day}, AMOUNT: USD{-largest_deficit}")
+    
 
 # This line changes the directory from the operating system back to the original.
 os.chdir(original_working_dir)
